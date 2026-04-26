0dd63e0a7b208125c9ded67e0c07e4891c60af13
JoaoGabrielSilva0205 <jgrmss@gmail.com>
2026-04-26 23:50:54 +0100

    Update ValidationRecord.java

--- src/ValidationRecord.java
+++ src/ValidationRecord.java
@@ -1,1 +1,17 @@
+import java.util.Date;
 
+class ValidationRecord {
+    private String name;
+    private String status;
+    private Date timestamp;
+
+    public ValidationRecord(String name, String status, Date timestamp) {
+        this.name = name;
+        this.status = status;
+        this.timestamp = timestamp;
+    }
+
+    public String toString() {
+        return name + " | " + status + " | " + timestamp;
+    }
+}
