62d050ebe64dab4dbbfd5b89ce831a1f98c1c819
JoaoGabrielSilva0205 <jgrmss@gmail.com>
2026-04-26 23:51:18 +0100

    Update LogEntry.java

--- src/LogEntry.java
+++ src/LogEntry.java
@@ -1,1 +1,15 @@
+import java.util.Date;
 
+class LogEntry {
+    private Date timestamp;
+    private String action;
+
+    public LogEntry(Date timestamp, String action) {
+        this.timestamp = timestamp;
+        this.action = action;
+    }
+
+    public String toString() {
+        return timestamp + " | " + action;
+    }
+}
